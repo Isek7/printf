5f57a777d27920b30ef431848442ac6a116fe486
Isek7 <bengabrielisek@gmail.com>
2023-08-22 12:25:58 -0700

    Update

---
+++ formater.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * handleHexLower - Handles lowercase hex
+ * @args: Args
+ * @pchar: pointer to number of printed characters
+ *
+ */
+void handleHexLower(va_list args, int *pchar)
+{
+	unsigned int num = va_arg(args, unsigned int);
+	*pchar += print_hex(num, 0);
+}
+
+/**
+ * handleHexUpper - Handles uppercase hex
+ * @args: Args
+ * @pchar: pointer to number of printed characters
+ *
+ */
+void handleHexUpper(va_list args, int *pchar)
+{
+	unsigned int num = va_arg(args, unsigned int);
+	*pchar += print_hex(num, 1);
+}
